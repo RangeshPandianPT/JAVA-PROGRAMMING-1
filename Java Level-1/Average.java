2ec4f3da4444c8f1702bfa27ecac4b213e65fd5b
RangeshPandian PT <rangeshpandian@gmail.com>
2025-05-04 11:57:55 +0530

    Add files via upload

---
+++ Java Level-1/Average.java
@@ -0,0 +1,12 @@
+public class Average {
+    public static void main(String[] args) {
+       
+        int physics = 95;
+        int chemistry = 96;
+		int math = 94;
+		int sum = physics+chemistry+math;
+		double average = sum / 3;
+	
+        System.out.println("Sam’s average mark in PCM is" + average );
+    }
+} 
